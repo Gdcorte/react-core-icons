380440c992277897d2a730343cf5bd5a6b7da34d
Gustavo Diniz da Corte <gustavodinizdacorte@Gustavos-MacBook-Pro.local>
2022-12-28 23:38:14 +0900

    health

---
+++ src/Health/Bundle.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { BoxedMultipleDecorator } from "../../.storybook/decorators";
+import SvgBox from "../Base/Box";
+import { IconComponent } from "../Base/interface";
+import HealthIcons from "./Bundle";
+
+export default {
+  title: "Bundles/Health",
+  decorators: [BoxedMultipleDecorator],
+} as ComponentMeta<IconComponent>;
+
+const Template: ComponentStory<IconComponent> = (args) => {
+  return (
+    <>
+      <SvgBox>
+        <HealthIcons.Healthy />
+      </SvgBox>
+      <SvgBox>
+        <HealthIcons.HeartRate />
+      </SvgBox>
+    </>
+  );
+};
+
+export const Health = Template.bind({});
---
+++ src/Health/index.tsx
@@ -0,0 +1,3 @@
+export { default as Health } from "./Bundle";
+export { default as HealthHealthy } from "./Healthy";
+export { default as HealthHeartRate } from "./HeartRate";
