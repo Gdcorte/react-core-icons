1082fe0eb257ae98ca33d0c431e0cc53448b3b55
Gustavo Diniz da Corte <gustavodinizdacorte@Gustavos-MacBook-Pro.local>
2022-12-28 23:26:06 +0900

    Gym

---
+++ src/Gym/Bundle.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { BoxedMultipleDecorator } from "../../.storybook/decorators";
+import SvgBox from "../Base/Box";
+import { IconComponent } from "../Base/interface";
+import GymIcons from "./Bundle";
+
+export default {
+  title: "Bundles/Gym",
+  decorators: [BoxedMultipleDecorator],
+} as ComponentMeta<IconComponent>;
+
+const Template: ComponentStory<IconComponent> = (args) => {
+  return (
+    <>
+      <SvgBox>
+        <GymIcons.Dumbbell />
+      </SvgBox>
+    </>
+  );
+};
+
+export const Gym = Template.bind({});
---
+++ src/Gym/Dumbbell.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { BoxedDecorator } from "../../.storybook/decorators";
+import { IconComponent } from "../Base/interface";
+import DumbbellIcon from "./Dumbbell";
+
+export default {
+  title: "Gym/Dumbbell",
+  component: DumbbellIcon,
+  decorators: [BoxedDecorator],
+} as ComponentMeta<IconComponent>;
+
+const Template: ComponentStory<IconComponent> = (args) => {
+  return <DumbbellIcon {...args} />;
+};
+
+export const Dumbbell = Template.bind({});
